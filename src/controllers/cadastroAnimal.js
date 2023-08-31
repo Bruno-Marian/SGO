7baa61972831353520ca3c3e46c2684a5589cb6a
BrunoSM <bruno.marian.info@gmail.com>
2023-08-30 21:25:04 -0300

    Realizado ajustes para adicionar dados faltantes

--- src/controllers/cadastroAnimal.js
+++ src/controllers/cadastroAnimal.js
@@ -1,7 +1,25 @@
 function addAnimal(animal){
     console.log("passou")
-    listaAnimal.push({nome: animal.nome, idade: animal.idade, especie: animal.especie})
+    listaAnimal.push({nome: animal.nome, 
+        idade: animal.idade, 
+        especie: animal.especie,
+        sexo: animal.sexo,
+        porte: animal.porte,
+        saude: animal.saude})
     console.log(listaAnimal)
 }
 
-const listaAnimal = [{nome: "Miu", especie: "Gato"}, {nome: "Minerva", especie: "Gato"}]
+const listaAnimal = [
+    {nome: "Miu", 
+    idade: '1.5', 
+    especie: "Gato",
+    sexo: "Feminino",
+    porte: "Pequeno",
+    saude: "Boa"}, 
+    {nome: "Minerva", 
+    idade: '2.5', 
+    especie: "Gato",
+    sexo: "Feminino",
+    porte: "Pequeno",
+    saude: "Boa"}, 
+]
