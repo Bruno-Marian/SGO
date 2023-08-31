26154af52b4404fb7126cb076814e6610afe1f25
BrunoSM <bruno.marian.info@gmail.com>
2023-08-30 21:18:21 -0300

    Realizado merge e implementado melhorias

--- src/controllers/cadastroAnimal.js
+++ src/controllers/cadastroAnimal.js
@@ -1,7 +1,7 @@
 function addAnimal(animal){
     console.log("passou")
-    listaAnimal.push({nome: animal.nome, tipo: animal.tipo})
+    listaAnimal.push({nome: animal.nome, idade: animal.idade, especie: animal.especie})
     console.log(listaAnimal)
 }
 
-const listaAnimal = [{nome: "Miu", tipo: "Gato"}, {nome: "Minerva", tipo: "Gato"}]
+const listaAnimal = [{nome: "Miu", especie: "Gato"}, {nome: "Minerva", especie: "Gato"}]
