453c05d80f38db2541cc09b2c7f9019a4345aa4c
BrunoSM <62313823+Bruno-Marian@users.noreply.github.com>
2023-09-16 13:45:30 -0300

    Realizado edições e melhorias no cadastro de animais

--- src/controllers/cadastroAnimal.js
+++ src/controllers/cadastroAnimal.js
@@ -1,72 +1,117 @@
 function addAnimal(animal) {
-    console.log("passou")
-    listaAnimal.push({
-        nome: animal.nome,
-        idade: animal.idade,
-        especie: animal.especie,
-        sexo: animal.sexo,
-        porte: animal.porte,
-        saude: animal.saude
-    })
-    console.log(listaAnimal)
-}
+    //Valida as informações do animal
+    if (!validateAnimal(animal)) return false;
 
-function retonarIdadeString(idade) {
-    idadeStr = idade.toString().split(".")
-    if (idadeStr.length > 1)
-        return `${idadeStr[0]} ano(s) e ${idadeStr[1]} mês(es)`
-    else if (idadeStr != "")
-        return `${idadeStr[0]} ano(s)`
-
-    return idade
-}
-
-function retornaTableAnimal() {
-    var tipoList = ["Nome", "Idade", "Espécie", "Sexo", "Porte", "Saúde"]
-    var html = "<table border='1|1'>";
-    html += "<tr>";
-    for (let i = 0; i < tipoList.length; i++) {
-        html += `<th>${tipoList[i]}</th>`;
+    let listaAnimal = retornaAnimal();
+    let id = null;
+    if(animal.id == null || animal.id == "null"){
+        if (listaAnimal.length == 0) {
+            id = 1;
+        }
+        else{
+            id = Math.max.apply(Math, listaAnimal.map(object => {
+                return object.id;
+            }));
+            id++;
+        }
     }
-    html += "</tr>";
-    for (var i = 0; i < listaAnimal.length; i++) {
-        html += "<tr>";
-        html += `<td>${listaAnimal[i].nome}</td>`;
-        html += `<td>${listaAnimal[i].idade}</td>`;
-        html += `<td>${listaAnimal[i].especie}</td>`;
-        html += `<td>${listaAnimal[i].sexo}</td>`;
-        html += `<td>${listaAnimal[i].porte}</td>`;
-        html += `<td>${listaAnimal[i].saude}</td>`;
-        html += "</tr>";
-    }
-    html += "</table>";
 
-    return html;
+    var animalResult = buscaAnimal(animal.id)
+    if (animalResult != -1) {
+        listaAnimal[animalResult] = animal
+    }else {
+        listaAnimal.push({
+            id: id,
+            nome: animal.nome,
+            idade: animal.idade,
+            especie: animal.especie,
+            sexo: animal.sexo,
+            porte: animal.porte,
+            saude: animal.saude,
+            disponivel: animal.disponivel
+        })
+    }
+    localStorage.setItem("animalList", JSON.stringify(listaAnimal))
 }
 
-const listaAnimal = [
-    {
-        nome: "Miu",
-        idade: '1.5',
-        especie: "Gato",
-        sexo: "Feminino",
-        porte: "Pequeno",
-        saude: "Boa, mais revoltado com a Minerva."
-    },
-    {
-        nome: "Minerva",
-        idade: '2.5',
-        especie: "Gato",
-        sexo: "Feminino",
-        porte: "Pequeno",
-        saude: "Boa, mais com medo da sombra."
-    },
-    {
-        nome: "Tigresa",
-        idade: '2.5',
-        especie: "Gato",
-        sexo: "Feminino",
-        porte: "Pequeno",
-        saude: "Boa, mas acima do peso."
-    },
-]
+retornaAnimal()
+function retornaAnimal() {
+    let animal = localStorage.getItem('animalList');
+
+    if (animal == undefined) {
+        return [];
+    }else{
+        return JSON.parse(animal);
+    } 
+}
+
+function buscaAnimal(id) {
+    let animal = retornaAnimal();
+
+    if (animal == undefined) {
+        return null;
+    }else{
+        return animal.findIndex(obj => obj.id == id);
+    } 
+}
+
+function addNewAnimal(isAnimal) {
+    if (isAnimal) {
+        const animal = {
+            "id": animalEdit,
+            "nome": nome.value,
+            "idade": idade.value,
+            "especie": document.querySelector("input[type='radio'][name=especie]:checked").value,
+            "sexo": document.querySelector("input[type='radio'][name=sexo]:checked").value,
+            "porte": porte.value,
+            "saude": saude.value,
+            "disponivel": true,
+        }
+        if (addAnimal(animal)){
+            reloadPage()
+        }
+    }
+}
+
+function reloadPage() {
+    window.location.reload();
+}
+
+function loadAnimal(id) {
+    let animalList = retornaAnimal()
+    console.log(animalList)
+    let animal = animalList.filter(obj => obj.id == id)[0]
+    
+    console.log(animal.especie)
+    console.log(animal)
+    if (animal){
+        nome.value = animal.nome
+        idade.value = animal.idade
+        porte.value = animal.porte
+        saude.value = animal.saude
+
+        if (animal.especie == "Gato"){
+            document.getElementById("gato").checked = true
+        }else{
+            document.getElementById("cachorro").checked = true
+        }
+
+        if (animal.sexo == "Macho"){
+            document.getElementById("macho").checked = true
+        }else{
+            document.getElementById("femea").checked = true
+        }
+    }
+}
+
+function validateAnimal(animal) {
+    obrigatorioList = ["nome", "idade"]
+    for (var i in obrigatorioList){
+        if (animal[obrigatorioList[i]] == "" || animal[obrigatorioList[i]] == null){
+            alert(`Campo obrigatório ${obrigatorioList[i]} não informado`)
+            return false;
+        }
+    }
+
+    return true;
+}
---
+++ src/controllers/listAnimal.js
@@ -0,0 +1,67 @@
+function retornaTableAnimal() {
+    var tipoList = ["Nome", "Idade", "Espécie", "Sexo", "Porte", "Saúde", "Editar", "Excluir"]
+    var html = "<table border='1|1'>";
+    html += "<tr>";
+    for (let i = 0; i < tipoList.length; i++) {
+        html += `<th>${tipoList[i]}</th>`;
+    }
+    let listaAnimal = retornaAnimal();
+    html += "</tr>";
+    for (var i = 0; i < listaAnimal.length; i++) {
+        html += "<tr>";
+        html += `<td>${listaAnimal[i].nome}</td>`;
+        html += `<td>${retonarIdadeString(listaAnimal[i].idade)}</td>`;
+        html += `<td>${listaAnimal[i].especie}</td>`;
+        html += `<td>${listaAnimal[i].sexo}</td>`;
+        html += `<td>${listaAnimal[i].porte}</td>`;
+        html += `<td>${listaAnimal[i].saude}</td>`;
+        html += `<td><button id=${listaAnimal[i].id} onclick="editAnimal(this.id)" class="btn btn-primary">Editar</button></td>`;
+        html += `<td><button id=${listaAnimal[i].id} onclick="deleteAnimal(this.id)" class="btn btn-danger">Excluir</butoon></td>`;
+        html += "</tr>";
+    }
+    html += "</table>";
+
+    return html;
+}
+
+function retonarIdadeString(idade) {
+    idadeStr = idade.toString().split(".")
+    if (idadeStr.length > 1)
+        return `${idadeStr[0]} ano(s) e ${idadeStr[1]} mês(es)`
+    else if (idadeStr != "")
+        return `${idadeStr[0]} ano(s)`
+
+    return idade
+}
+
+function retornaAnimal() {
+    let animal = localStorage.getItem('animalList');
+
+    if (animal == undefined) {
+        return [];
+    }else{
+        return JSON.parse(animal)
+    } 
+}
+
+function deleteAnimal(id) {
+    let animalList = retornaAnimal()
+    let animal = animalList.filter(obj => obj.id == id)
+
+    if (animal){
+        animalList.pop(animal)
+    }
+    
+    localStorage.setItem("animalList", JSON.stringify(animalList))
+    window.location.reload()
+}
+
+function cadastraClick() {
+    let page = "cadastroAnimal.html";
+    document.body.innerHTML = `<object type="text/html" data="${page}"></object>`;
+}
+
+function editAnimal(id) {
+    localStorage.setItem("animalEdit", id);
+    cadastraClick();
+}
